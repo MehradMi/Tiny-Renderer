353371a22267aed120bde20826c2e2b5263ad87e
MehradMi <165255856+MehradMi@users.noreply.github.com>
2025-12-25 00:07:31 +0330

    Working on Bresenham's line drawing algorithm

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "./src/tga-image-api/tgaimage.h"
 #include <cmath>
+#include <utility>
 
 // NOTE: TGAColor uses BGRA order instead of RGBA
 constexpr TGAColor white = {255, 255, 255, 255};
@@ -8,12 +9,33 @@ constexpr TGAColor green = {0, 255, 0, 255};
 constexpr TGAColor red = {0, 0, 255, 255};
 constexpr TGAColor yellow = {0, 200, 255, 255};
 
-void line(int ax, int ay, int bx, int by, TGAImage &framebuffer,
-          TGAColor color) {
-  for (float t{0.f}; t < 1.f; t += .02) {
-    int x = std::round(ax + (bx - ax) * t);
-    int y = std::round(ay + (by - ay) * t);
-    framebuffer.set(x, y, color);
+void drawLine(int x0, int y0, int x1, int y1, TGAImage &framebuffer,
+              TGAColor color) {
+  const int dx = x1 - x0;
+  const int dy = y1 - y0;
+  bool steep = dy > dx;
+  if (steep) {
+    std::swap(x0, y0);
+    std::swap(x1, y1);
+  }
+  int decision = (dy << 1) - dx;
+  int y = y0;
+
+  for (int x{x0}; x <= x1; x++) {
+    if (steep) {
+      framebuffer.set(y, x, color);
+    } else {
+
+      framebuffer.set(x, y, color);
+    }
+    // decision: d0 - d1 -> Thus, if it is positive
+    // then the pixel above "p" must be colored.
+    if (decision > 0) {
+      y++;
+      decision += (dy - dx) << 1;
+    } else {
+      decision += dy << 1;
+    }
   }
 }
 
@@ -23,18 +45,20 @@ int main() {
   constexpr int height = 64;
   TGAImage framebuffer(width, height, TGAImage::RGB);
 
-  int ax = 3, ay = 7;
+  int ax = 7, ay = 3;
   int bx = 12, by = 37;
   int cx = 62, cy = 53;
 
+  drawLine(ax, ay, bx, by, framebuffer, blue);
+  drawLine(cx, cy, bx, by, framebuffer, green);
+  drawLine(cx, cy, ax, ay, framebuffer, yellow);
+  drawLine(ax, ay, cx, cy, framebuffer, red);
+
+  // NOTE: Here I color the vertices so they are clear in
+  // the output image.
   framebuffer.set(ax, ay, white);
   framebuffer.set(bx, by, white);
   framebuffer.set(cx, cy, white);
-
-  line(ax, ay, bx, by, framebuffer, blue);
-  line(cx, cy, bx, by, framebuffer, green);
-  line(cx, cy, ax, ay, framebuffer, yellow);
-  line(ax, ay, cx, cy, framebuffer, red);
 
   framebuffer.write_tga_file("framebuffer.tga");
 
